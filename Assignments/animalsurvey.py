f6c33ccafc3e89f2b9dd1edf24ebaa8dc3348bef
AJEngebretson2027 <821093@mystma.org>
2024-09-25 09:29:38 -0500

    Adding comments to animal survey

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,6 +1,6 @@
-favorite_animal = input("what is your favorite animal?\n>")
-detail_1 = input("why do you like them?\n>")
-detail_2 = input("where do they live?\n>")
-detail_3 = input("what is a fun fact about them?\n>")
-detail_4 = input("what color are they?\n>")
-print("Your favorite animal are " + favorite_animal + " because " + detail_1 + ". They live in " + detail_2 + " and " + detail_3 + ". They are "+ detail_4 + ".")
+favorite_animal = input("what is your favorite animal?\n>")     # Asking what their favorite animal is
+detail_1 = input("why do you like them?\n>")    # Asking why they like the animal they choose
+detail_2 = input("where do they live?\n>")      # Asking where they live
+detail_3 = input("what is a fun fact about them?\n>")   # Asking for a fun fact
+detail_4 = input("what color are they?\n>")     # Asking what color they are
+print("Your favorite animal are " + favorite_animal + " because " + detail_1 + ". They live in " + detail_2 + " and " + detail_3 + ". They are "+ detail_4 + ".")   # Turning all the information into a summary
--- Unit 2 - Functions and Variables/Sept25thPractice.py
+++
@@ -1,3 +0,0 @@
-x = 4
-x = int(x)
-print(x+3)
