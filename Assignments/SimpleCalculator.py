fe25f6a911d9c3758493d383264eb338cf06eb24
AJEngebretson2027 <821093@mystma.org>
2024-09-30 09:36:58 -0500

    creating new files

---
+++ Assignments/SimpleCalculator.py
@@ -0,0 +1,5 @@
+def add():
+    print("add two numbers:")
+    x = input("What is the first number?\n>")
+    y = input("What is the seconds number?\n>")
+    print(str(x) + " + " + str(y) + " = " + str(x + y))
---
+++ Unit 2 - Functions and Variables/CalculateTax.py
@@ -0,0 +1,4 @@
+def calculate_tax(item, price, rate): #define fuction
+    print("A " + item + " that costs $" + str(price) + " has a tax of " + str(price * rate))
+
+calculate_tax("baseball", 4.99, 0.06875) #run the function
