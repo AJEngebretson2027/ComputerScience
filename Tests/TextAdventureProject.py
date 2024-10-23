f6d4c5ea0557a3abb200f1e482abe0cb919f6942
AJEngebretson2027 <821093@mystma.org>
2024-10-23 09:54:07 -0500

    testing and text adventure progress

--- Tests/TextAdventureProject.py
+++ Tests/TextAdventureProject.py
@@ -2,11 +2,13 @@
 # i was originally going to have you choose between my different party members, but that would be too complitcated
 # you are starting after you take a job from cortland to escort a caravan for a week to Carewick
 
+health = 10
+
 import random
 
 def adv_start():
     print("You start out around a caravan, escorting it to the nearby town of Carewick.")
-    print("You are Anthony Spellhardt, a swordsage based aroun the Desert Wind school (fire)")
+    print("You are Anthony Spellhardt, a swordsage wielding a halberd and based aroun the Desert Wind school (fire)")
     print("nothing happens during the day, and for tonight you were choosen to keep watch for a few hours")
 
     print("You stand guard listening and watching during the night, thankfully you have dark vision which allows you to see during the night, although limited")
@@ -33,7 +35,7 @@ def adv_start():
         wolf_ambush
     
 def spot_wolf():
-    print("You are both staring at each other. What you you do?")
+    print("You are both staring at each other. What you you do?")       #different options you can choose, may lead to weaker or stronger encounters
     print("1. Scare off the wolf by yelling")
     print("2. attempt to attack the wolf")
     print("3 try to befriend the wolf")
@@ -51,28 +53,84 @@ def spot_wolf():
             AoO = input(">")
             AoO = int(AoO)
             if AoO == 1:
-                attack_AoO = (random.randint(1,20))
-                print ("you rolled " + str(attack_AoO + 6) + "for attack")
-                if attack_AoO >= 14:
-                    damage_AoO = (random.randint(1,10))
-                    print("you hit the wolf and deal " + str(damage_AoO + 4) + "points of damage")      #im just now realizing how many nested if statements i need for this
-                    if damage_AoO == 14:
-                        print("you kill the wolf before it can return to its pack.")
-                    else:
-                        print("you injure the wolf, but it manages to escape.")
-                elif attack_AoO < 13:
-                    print ("you miss the wolf as it runs away, back to its pack")
-                    str_wolf_ambush
+                attack_wolf
 
             elif AoO == 2:
                 print("the wolf runs off.")
                 wolf_ambush
+
     if choice == 2:
         print("as you start to move up to attack the wolf, it runs off before you can hit it.")
         wolf_ambush
 
+
     if choice == 3:
         print("you bend down and hold out your hand, allowing it to sniff it. it sniffs a bit before running off")
-        weak_wolf_ambush
-    
+        wolf_ambush
+
+def attack_wolf():
+    attack_AoO = (random.randint(1,20))
+    print ("you rolled " + str(attack_AoO + 6) + "for attack")
+
+    if attack_AoO >= 14:
+            damage_AoO = (random.randint(1,10))
+            print("you hit the wolf and deal " + str(damage_AoO + 4) + "points of damage")      #im just now realizing how many nested if statements i need for this
+            if damage_AoO == 14:
+                print("you kill the wolf before it can return to its pack.")
+                wolf_ambush
+
+            else:
+                print("you injure the wolf, but it manages to escape.")
+                wolf_ambush
+    elif attack_AoO < 13:
+                    print ("you miss the wolf as it runs away, back to its pack")
+                    wolf_ambush
+
+def hear_something():
+     print("What would you like to do?")
+     print("1. investigate the noise")
+     print("2. ignore the noise, must've been the wind")    #skyrim reference
+
+     heard_something = input(">")
+     heard_something = int(heard_something)
+     if heard_something == 1:
+          print("you walk over to where you saw the noise, and you see a wolf.")
+          spot_wolf
+     elif heard_something == 2:
+          print("you decided it is nothing, continuing your watch.")
+          wolf_ambush
+
+def wolf_ambush():
+     print("You continue on with your night, and eventually switchng places with ashmook, the party's monk.")
+     print("but before you can even take off your armor, you hear howling of wolfs behind your camp")
+     print("you quickly get out your tent, as you see 5 wolfs readying their attack")
+     print("you react fast, what do you do?")
+     print("1. charge the first wolf you see")
+     print("2. move up and yell to attract the wolfs")
+     print("3. wake up your party members")
+     choice2 = input(">")
+     choice2 = int(choice2)
+     if choice2 == 1:
+          attack_wolf2
+        
+     elif choice2 == 2:
+          attract_wolfs
+
+     elif choice2 == 3:
+        wake_party
+
+def attack_wolf2():
+    print("you cast burning blade before striking the wolf")
+    charge_wolf = (random.randint(1,20))
+    print("you rolled " + str(charge_wolf + 8) + " for your attack")
+
+    if charge_wolf >= 14:
+         print("you manage to hit the wolf")
+         charge_damage = (random.randint(1,10))
+         bb_damage = (random.randint(1,6))
+         total_charge = charge_damage + bb_damage
+
+         print("you hit the wolf, dealing " + str(total_charge + 4) + " damage")
+         if total_charge >= 14:
+              print("you instantly split the wolf in half, killing it")
 
---
+++ Unit 3 - Conditionals and Exceptions/randomTesting.py
@@ -0,0 +1,5 @@
+import random
+d = (random.randint(1,10))
+d2 = (random.randint(1,6))
+t = d + d2
+print(t)
