1952d062f732544bcf5580ae77e07454e49a7a05
AJEngebretson2027 <821093@mystma.org>
2024-11-21 08:58:45 -0600

    fixing tippy

--- Circuit Playground/tippy.py
+++ Circuit Playground/tippy.py
@@ -1,7 +1,14 @@
 from adafruit_circuitplayground import cp
 import time
 while True:
-   for i in range(1,3):
-      cp.pixels[i] = (0,10,0)
-   for n in range(6,8):
-      cp. pixels[n] = (0,0,0)
+    x, y, z = cp.acceleration
+    if x > 1:
+        for i in range(1,4):
+            cp.pixels[i] = (0,0,10)
+        for n in range(6,9):
+            cp.pixels[n] = (0,0,0)
+    elif x < -1:
+        for i in range(1,4):
+            cp.pixels[i] = (0,0,0)
+        for n in range(6,9):
+            cp.pixels[n] = (0,0,10)
